fbd1a1a1bdf852c3787b16010fd291e87cb2b618
Ozan Ergüleç <ozanergulec@hotmail.com>
2026-01-27 16:21:01 +0300

    DB migration added for docker initialization

--- src/ResearchNetwork.API/Program.cs
+++ src/ResearchNetwork.API/Program.cs
@@ -84,6 +84,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Auto-migrate database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
